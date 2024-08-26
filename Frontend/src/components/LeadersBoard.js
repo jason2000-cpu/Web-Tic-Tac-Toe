d25d6fcb49f488e5e838524950406665d2605882
David Nanjila <nanjiladavid2@gmail.com>
2024-08-26 21:57:02 +0300

    Chore: Style the leaderboard page
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

--- Frontend/src/components/LeadersBoard.js
+++ Frontend/src/components/LeadersBoard.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react';
 
 function LeaderBoard() {
     const [players, setPlayers] = useState([
-        { rank: 1, username: 'David', score: 100 },
-        { rank: 2, username: 'John', score: 90 },
-        { rank: 3, username: 'Jane', score: 80 }
+        { rank: 'One', username: 'David', score: 100 },
+        { rank: 'Two', username: 'John', score: 90 },
+        { rank: 'Three', username: 'Jane', score: 80 }
     ]);
 
     return (
         <div className="max-w-4xl mx-auto p-6 bg-red-800 rounded-lg shadow-md">
             <h1 className="text-2xl font-semibold text-center mb-4 text-white">Leader Board</h1>
-            <table className="w-full bg-white border border-gray-200 rounded-lg shadow-md">
+            <table className="w-full bg-red-800 border-gray-200 rounded-lg ">
                 <thead className="bg-gray-200 text-gray-700">
                     <tr>
-                        <th className="py-3 px-4 text-left">Rank</th>
-                        <th className="py-3 px-4 text-left">Username</th>
-                        <th className="py-3 px-4 text-left">Score</th>
+                        <th className="py-3 px-4 text-center">Rank</th>
+                        <th className="py-3 px-4 text-center">Username</th>
+                        <th className="py-3 px-4 text-center">Points</th>
                     </tr>
                 </thead>
-                <tbody className="text-gray-600">
+                <tbody className="text-white-600">
                     {players.map(player => (
                         <tr key={player.rank} className="border-b border-gray-200 hover:bg-gray-50">
                             <td className="py-2 px-4">{player.rank}</td>
